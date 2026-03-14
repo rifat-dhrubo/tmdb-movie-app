a3126698848677f0594f952ccb9d845af807db08
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2026-03-14 15:59:09 +0600

    Initial commit

---
+++ src/integrations/tanstack-query/devtools.tsx
@@ -0,0 +1,7 @@
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
+
+const config = {
+	name: 'Tanstack Query',
+	render: <ReactQueryDevtoolsPanel />,
+};
+export { config };
---
+++ src/routes/__root.tsx
@@ -0,0 +1,30 @@
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import type { QueryClient } from '@tanstack/react-query';
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+
+import { config } from '../integrations/tanstack-query/devtools';
+
+interface MyRouterContext {
+	queryClient: QueryClient;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+	component: () => (
+		<>
+			<Outlet />
+			<TanStackDevtools
+				config={{
+					position: 'bottom-right',
+				}}
+				plugins={[
+					{
+						name: 'Tanstack Router',
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+					config,
+				]}
+			/>
+		</>
+	),
+});
